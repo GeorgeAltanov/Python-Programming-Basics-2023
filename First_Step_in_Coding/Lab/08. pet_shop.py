927ad233ebd980e570af2793796b1feaf60a32c7
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-03-06 21:07:47 +0200

    Create 08. pet_shop.py

---
+++ First_Step_in_Coding/Lab/08. pet_shop.py
@@ -0,0 +1,8 @@
+dogs_food = int(input())
+cats_food = int(input())
+
+price_dogs_food = dogs_food * 2.50
+price_cats_food = cats_food * 4
+total_price = price_dogs_food + price_cats_food
+
+print(f"{total_price} lv.")
