c2954d69be85f097b274a0cb6ba23df7ca52430c
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-03-06 21:08:25 +0200

    Create 09. yard_greening.py

---
+++ First_Step_in_Coding/Lab/09. yard_greening.py
@@ -0,0 +1,8 @@
+green_area = float(input())
+
+price_green_area = green_area * 7.61
+discount = price_green_area * 0.18
+final_price = price_green_area - discount
+
+print(f"The final price is: {final_price} lv.")
+print(f"The discount is: {discount} lv.")
