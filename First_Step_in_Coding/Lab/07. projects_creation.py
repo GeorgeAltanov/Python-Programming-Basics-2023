7c7965811360f164443b285262bea55316d04c35
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-03-06 21:07:20 +0200

    Create 07. projects_creation.py

---
+++ First_Step_in_Coding/Lab/07. projects_creation.py
@@ -0,0 +1,6 @@
+name_architect = input()
+quantity_projects = int(input())
+
+hours = quantity_projects * 3
+
+print(f"The architect {name_architect} will need {hours} hours to complete {quantity_projects} project/s.")
