312be50972400f28eba3970957150db067c8bb4a
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-03-17 22:01:21 +0200

    Create 06. movie_destination.py

---
+++ Conditional_Statements_Advanced/Exams_Exercises/06. movie_destination.py
@@ -0,0 +1,28 @@
+budget = float(input())
+destination = input()
+season = input()
+days = int(input())
+
+total_price = 0
+
+if season == "Winter":
+    if destination == "Dubai":
+        total_price = days * 45000 * 0.70
+    elif destination == "Sofia":
+        total_price = days * 17000 * 1.25
+    elif destination == "London":
+        total_price = days * 24000
+
+elif season == "Summer":
+    if destination == "Dubai":
+        total_price = days * 40000 * 0.70
+    elif destination == "Sofia":
+        total_price = days * 12500 * 1.25
+    elif destination == "London":
+        total_price = days * 20250
+
+if budget >= total_price:
+    print(f"The budget for the movie is enough! We have {budget - total_price:.2f} leva left!")
+
+else:
+    print(f"The director needs {total_price - budget:.2f} leva more!")
